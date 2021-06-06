470bbf569c61c6a0930645a00f0ea190f236285c
dekapaan <54145779+dekapaan@users.noreply.github.com>
2021-06-06 19:13:21 +0200

    Update exception_handling.py

--- exception_handling.py
+++ exception_handling.py
@@ -1,103 +1,46 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5 import QtCore
-import sys
-import qtawesome as qta
+from tkinter import *
+from tkinter import messagebox
 
 
-class ExceptionHandling(QWidget):
-    def __init__(self):
+class ExceptionHandleGUI:
+    def __init__(self, master):
+        # Window setup
+        self.master = master
+        self.master.geometry('300x120')
+        self.master.title('Exception Handling')
+        self.master.config(bg='#222')
 
-        super().__init__()
-        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
-        self.setFixedWidth(400)
-        self.setFixedHeight(200)
-        self.setStyleSheet(
-            """
-            * {background: #222;}
+        # Amount in account label and entry
+        self.lbl_amount = Label(self.master, text='Please enter amount in you account', font='monospace 10', bg='#222',
+                                fg='white')
+        self.entry_amount = Entry(self.master, width=21)
+        self.lbl_amount.pack()
+        self.entry_amount.pack()
 
-            QLineEdit {
-                background: white; 
-                color: #222;
-                border-radius: 2px;
-                width: 300px;
-                height: 30px;
-            }
-            """
-        )
-        self.title = QLabel('Exception handling', self)
-        self.title.setStyleSheet("background: #222;" + "color: white;")
-        self.title.move(12, 11)
+        # Verification button
+        self.btn_verify = Button(self.master, text='Check qualification', bg='#FF69B4', font='monospace 10', width=18,
+                                 command=self.qualify)
+        self.btn_verify.pack()
 
-        self.icon_exit = qta.icon('mdi.close', color='white', scale_factor=2)
-        self.btn_exit = QPushButton(self.icon_exit, '', self)
-        self.btn_exit.setCursor(QtCore.Qt.PointingHandCursor)
-        self.btn_exit.clicked.connect(exit)
-        self.btn_exit.setStyleSheet("background: #222;" + "border: none;" + "color: white;")
-        self.btn_exit.move(370, 12)
+        # Show window
+        self.master.mainloop()
 
-        self.entry_amount = QLineEdit(self)
-        self.entry_amount.setPlaceholderText("Enter amount in your account")
-        self.entry_amount.move(50, 70)
-
-        self.btn_login = QPushButton('Check Qualification', self)
-        self.btn_login.clicked.connect(self.check)
-        self.btn_login.setStyleSheet(
-            """
-            * {
-                background: #222;
-                color: #ff007f;
-                border: 1px solid #ff007f;
-                padding: 10px 30px;
-                border-radius: 2px;
-            }
-
-            :hover {
-                background: #ff007f;
-                color: #222;
-            }
-            """)
-        self.btn_login.move(100, 120)
-
-        self.show()
-
-        self.old_pos = self.pos()
-
-    def mousePressEvent(self, event):
-        self.old_pos = event.globalPos()
-
-    def mouseMoveEvent(self, event):
-        moved = QtCore.QPoint(event.globalPos() - self.old_pos)
-        self.move(self.x() + moved.x(), self.y() + moved.y())
-        self.old_pos = event.globalPos()
-
-    def check(self):
+    def qualify(self):      # Checks if amount is over or under 3000 to see if you qualify for malaysia trip
         try:
-            amount = int(self.entry_amount.text())
-            if amount < 0:
+            amount = int(self.entry_amount.get())  # gets amount
+            if amount < 0:  # if value is negative, raise exception
                 raise ValueError
 
             if amount < 3000:
-                msg_box = QMessageBox()
-                msg_box.setText("Insufficient Amount")
-                msg_box.setIcon(QMessageBox.Critical)
-                msg_box.setWindowTitle("Warning")
-                msg_box.exec()
+                messagebox.showerror('Sorry', 'You do not qualify to go to Malaysia')
 
-            elif amount >= 3000:
-                msg_box = QMessageBox()
-                msg_box.setText("Congratulations. You qualify to go to Malaysia")
-                msg_box.setIcon(QMessageBox.Information)
-                msg_box.setWindowTitle("Good News")
-                msg_box.exec()
+            if amount >= 3000:
+                messagebox.showinfo('Good news', 'Congratulations. You qualify to go to Malaysia')
 
-        except ValueError:
-            msg_box = QMessageBox()
-            msg_box.setText("Invalid amount")
-            msg_box.setIcon(QMessageBox.Warning)
-            msg_box.setWindowTitle("Error")
-            msg_box.exec()
+        except ValueError:  # used to trap invalid variable type and negative amounts
+            messagebox.showwarning(message='Invalid amount')
 
 
-app = QApplication(sys.argv)
-root = ExceptionHandling()
+# instantiation
+member_area = Tk()
+ExceptionHandleGUI(member_area)
